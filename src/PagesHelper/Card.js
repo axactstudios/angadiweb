e63656fd630b1224b065f6f0c0db741936f86380
akashVerma <akashv000@gmail.com>
2020-11-11 12:56:31 +0530

    card

--- src/PagesHelper/Card.js
+++ src/PagesHelper/Card.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import '../Styles/pgcard.css'
 import { addItem, updateItem } from '../helpers/CartHelper'
 import { Redirect, Link } from 'react-router-dom'
+import { Form } from 'react-bootstrap'
 
 const Card = ({ product }) => {
     const [redirect, setRedirect] = useState(false)
     const [count, setCount] = useState(0);
+    const [quan, setquan] = useState(500)
 
 
     const handleChange = () => e => {
@@ -28,6 +30,10 @@ const Card = ({ product }) => {
         }
     }
 
+    const handleChanged = () => (e) => {
+        setquan(e.target.value)
+    };
+
     return (
         <div className="csk">
             {shouldRedirect(redirect)}
@@ -37,11 +43,23 @@ const Card = ({ product }) => {
                 </Link>
             </div>
             <div className="csk2">
-                <p>{product.data.name}</p>
-                <li>iPrice: {product.data.iPrice}</li>
-                <li>price: {product.data.price}</li>
-                <input type='number' onChange={handleChange()} value={count} className='form-control' />
-                <button onClick={addToCart}>Add</button>
+                <h6>{product.data.name}</h6>
+                <p>{product.data.category}</p>
+                <div className='selectcard2'>
+                    <select onChange={handleChanged()} className="slectcard">
+                        <option value='500'>500 ML</option>
+                        <option value='1000'>1000 ML</option>
+                        <option value='1500'>1500 ML</option>
+                        <option value='2000'>2000 ML</option>
+                    </select>
+                </div>
+                <p>Rs {product.data.price}  <span>Rs {product.data.iPrice}</span> </p>
+                <div className="inputdiv">
+                <input type='number' onChange={handleChange()} value={count} className='cartinputt' />
+                </div>
+                <div className="cardbuttondiv">
+                <button className='cardbutton' onClick={addToCart}>Add</button>
+                </div>
             </div>
         </div>
     );
