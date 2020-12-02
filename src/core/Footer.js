22b946f81ba880010eb801af1e363621cfdbd013
akashVerma <akashv000@gmail.com>
2020-12-02 22:40:46 +0530

    footer ready

--- src/core/Footer.js
+++ src/core/Footer.js
@@ -4,6 +4,8 @@ import '../Styles/Footer.css'
 import IOS from '../Assests/IOS.svg'
 import PLAY from '../Assests/play.svg'
 import $ from 'jquery'
+import { Link } from 'react-router-dom'
+import { itemTotal } from '../helpers/CartHelper'
 
 const Footer = () => {
 
@@ -16,77 +18,113 @@ const Footer = () => {
     return (
         <>
             <div className="home15">
-                <div className="home16">
+                <div className='bheegi'>
+                    <div className="home16">
+                        <Container>
+                            <Row>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="home17">
+                                        <h3>Trending Dishes</h3>
+                                        <ul>
+                                            <li><a href="jdfakkl">XYZ</a></li>
+                                            <li><a href="jdfakkl">XYZ</a></li>
+                                            <li><a href="jdfakkl">Delhi</a></li>
+                                            <li><a href="jdfakkl">Kolkata</a></li>
+                                        </ul>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="home18">
+                                        <h3>About Us</h3>
+                                        <ul>
+                                            <li><a href="jdfakkl">About Angadi</a></li>
+                                            <li><a href="jdfakkl">Careers</a></li>
+                                            <li><a href="jdfakkl">Terms and Conditions</a></li>
+                                            <li><a href="jdfakkl">Privacy Policy</a></li>
+                                        </ul>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="home19">
+                                        <h3>Help & Support</h3>
+                                        <ul>
+                                            <li><a href="jdfakkl">Help</a></li>
+                                            <li><a href="jdfakkl">Contact Support</a></li>
+                                            <li><a href="jdfakkl">Contact Us</a></li>
+                                            <li><a href="jdfakkl">FAQ</a></li>
+                                        </ul>
+                                    </div>
+                                </Col>
+                                <Col xs={12} sm={6} md={4} lg={3}>
+                                    <div className="home20">
+                                        <Row className="text-center">
+                                            <Col sm={12} className="">
+                                                <h3>Follow Us</h3>
+                                            </Col>
+
+                                            <Col sm={4} xs={4} className="">
+                                                <a href="#" style={{ color: 'rgb(68, 67, 67)' }} className="btn btn-lg"><i className="fa fa-facebook"></i></a>
+
+                                            </Col>
+                                            <Col sm={4} xs={4} className="">
+                                                <a href="#" style={{ color: 'rgb(68, 67, 67)' }} className="btn btn-lg "><i className="fa fa-twitter"></i></a>
+                                            </Col>
+                                            <Col sm={4} xs={4} className="">
+                                                <a href="#" style={{ color: 'rgb(68, 67, 67)' }} className="btn btn-lg "><i className="fa fa-linkedin"></i></a>
+                                            </Col>
+                                            <Col sm={12} xs={5} className="text-center m-1">
+                                                <a href="#" className="btn m-0">
+                                                    <img src={IOS} style={{ widht: '100%' }} />
+                                                </a>
+                                            </Col>
+                                            <Col sm={12} xs={5} className="text-center m-1">
+                                                <a href="#" className="btn m-0">
+                                                    <img src={PLAY} style={{ widht: '100%' }} />
+                                                </a>
+                                            </Col>
+                                        </Row>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
+                    <div className="home21">
+                        <p>@2020, Angadi. All Rights Reserved</p>
+                    </div>
+                </div>
+                <div className='bhaagi'>
                     <Container>
                         <Row>
-                            <Col xs={12} sm={6} md={4} lg={3}>
-                                <div className="home17">
-                                    <h3>Trending Dishes</h3>
-                                    <ul>
-                                        <li><a href="jdfakkl">XYZ</a></li>
-                                        <li><a href="jdfakkl">XYZ</a></li>
-                                        <li><a href="jdfakkl">Delhi</a></li>
-                                        <li><a href="jdfakkl">Kolkata</a></li>
-                                    </ul>
+                            <Col><Link to='/' className='bhaagi2'>
+                                <div className='bhaagi1'>
+                                    <i class="fa fa-home" aria-hidden="true"></i>
+                                    <p>Home</p>
                                 </div>
+                            </Link>
                             </Col>
-                            <Col xs={12} sm={6} md={4} lg={3}>
-                                <div className="home18">
-                                    <h3>About Us</h3>
-                                    <ul>
-                                        <li><a href="jdfakkl">About Angadi</a></li>
-                                        <li><a href="jdfakkl">Careers</a></li>
-                                        <li><a href="jdfakkl">Terms and Conditions</a></li>
-                                        <li><a href="jdfakkl">Privacy Policy</a></li>
-                                    </ul>
+                            <Col><Link to='/cart' className='bhaagi2'>
+                                <div className='bhaagi1'>
+                                    <i class="fa fa-shopping-cart" aria-hidden="true"> <sup>{itemTotal()}</sup></i>
+                                    <p>Cart</p>
                                 </div>
+                            </Link>
                             </Col>
-                            <Col xs={12} sm={6} md={4} lg={3}>
-                                <div className="home19">
-                                    <h3>Help & Support</h3>
-                                    <ul>
-                                        <li><a href="jdfakkl">Help</a></li>
-                                        <li><a href="jdfakkl">Contact Support</a></li>
-                                        <li><a href="jdfakkl">Contact Us</a></li>
-                                        <li><a href="jdfakkl">FAQ</a></li>
-                                    </ul>
+                            <Col><Link to='/shop' className='bhaagi2'>
+                                <div className='bhaagi1'>
+                                    <i class="fa fa-search" aria-hidden="true"></i>
+                                    <p>Search</p>
                                 </div>
+                            </Link>
                             </Col>
-                            <Col xs={12} sm={6} md={4} lg={3}>
-                                <div className="home20">
-                                    <Row className="text-center">
-                                        <Col sm={12} className="">
-                                            <h3>Follow Us</h3>
-                                        </Col>
-
-                                        <Col sm={4} xs={4} className="">
-                                            <a href="#" style={{ color: 'rgb(68, 67, 67)' }} className="btn btn-lg"><i className="fa fa-facebook"></i></a>
-
-                                        </Col>
-                                        <Col sm={4} xs={4} className="">
-                                            <a href="#" style={{ color: 'rgb(68, 67, 67)' }} className="btn btn-lg "><i className="fa fa-twitter"></i></a>
-                                        </Col>
-                                        <Col sm={4} xs={4} className="">
-                                            <a href="#" style={{ color: 'rgb(68, 67, 67)' }} className="btn btn-lg "><i className="fa fa-linkedin"></i></a>
-                                        </Col>
-                                        <Col sm={12} xs={5} className="text-center m-1">
-                                            <a href="#" className="btn m-0">
-                                                <img src={IOS} style={{ widht: '100%' }} />
-                                            </a>
-                                        </Col>
-                                        <Col sm={12} xs={5} className="text-center m-1">
-                                            <a href="#" className="btn m-0">
-                                                <img src={PLAY} style={{ widht: '100%' }} />
-                                            </a>
-                                        </Col>
-                                    </Row>
+                            <Col><Link to='/user/dashboard' className='bhaagi2'>
+                                <div className='bhaagi1'>
+                                    <i class="fa fa-user" aria-hidden="true"></i>
+                                    <p>Dashboard</p>
                                 </div>
+                            </Link>
                             </Col>
                         </Row>
                     </Container>
-                </div>
-                <div className="home21">
-                    <p>@2020, Angadi. All Rights Reserved</p>
                 </div>
             </div>
         </>
--- src/core/Menu.js
+++ src/core/Menu.js
@@ -3,6 +3,7 @@ import { Link, withRouter } from 'react-router-dom'
 import Log from '../User/Logout'
 import '../Styles/menu.css'
 import { isAuth } from '../helpers/auth'
+import {itemTotal} from '../helpers/CartHelper'
 import * as firebase from 'firebase'
 import { Form } from 'react-bootstrap';
 
@@ -95,11 +96,11 @@ const Menu = ({ history }) => {
                 </div>
                 <div className="men3">
                     <ul className="navlink">
-                        {!isAuth() && <Link onClick={changeScreen} style={isActive(history, '/login')} to='/login'><li>Sign In</li></Link>}
+                        {!isAuth() && <Link onClick={changeScreen} style={isActive(history, '/login')} to='/login' className='bhaagi2'><li>Sign In</li></Link>}
                         {
-                          isAuth() && <Link onClick={changeScreen} style={isActive(history, `/user/dashboard`)} to={`/user/dashboard`}><li>Dashboard</li></Link>
+                          isAuth() && <Link onClick={changeScreen} style={isActive(history, `/user/dashboard`)} to={`/user/dashboard`} className='bhaagi2'><li>Dashboard</li></Link>
                         }
-                        <Link onClick={changeScreen} style={isActive(history, '/shop')} to='/shop'><li>Do It Yourself</li></Link>
+                        <Link onClick={changeScreen} style={isActive(history, '/shop')} to='/shop' className='bhaagi2'><li>Do It Yourself</li></Link>
 
                         {
                             cat &&
@@ -109,7 +110,7 @@ const Menu = ({ history }) => {
                                         <Fragment>
                                             {
                                                 l.data.catName === 'Pickles/Podi' &&
-                                                <Link onClick={changeScreen} style={isActive(history, `/shop/${l.data.catName}`)} to={`/shop/${l.data.catName}`}><li>{l.data.catName}</li></Link>
+                                                <Link onClick={changeScreen} style={isActive(history, `/shop/${l.data.catName}`)} to={`/shop/${l.data.catName}`} className='bhaagi2'><li>{l.data.catName}</li></Link>
                                             }
                                         </Fragment>
                                     ))
@@ -124,7 +125,7 @@ const Menu = ({ history }) => {
                                 <div className="dropdown-content">
                                     {
                                         cat.map((l, k) => (
-                                            <Link onClick={changeScreen} style={isActive(history, `/shop/${l.data.catName}`)} to={`/shop/${l.data.catName}`}>{l.data.catName}</Link>
+                                            <Link onClick={changeScreen} style={isActive(history, `/shop/${l.data.catName}`)} to={`/shop/${l.data.catName}`} className='bhaagi2'>{l.data.catName}</Link>
                                         ))
                                     }
                                 </div>
@@ -139,7 +140,7 @@ const Menu = ({ history }) => {
                                         <Fragment>
                                             {
                                                 l.data.catName === 'Curries/Koottu' &&
-                                                <Link onClick={changeScreen} style={isActive(history, `/shop/${l.data.catName}`)} to={`/shop/${l.data.catName}`}><li>{l.data.catName}</li></Link>
+                                                <Link onClick={changeScreen} style={isActive(history, `/shop/${l.data.catName}`)} to={`/shop/${l.data.catName}`} className='bhaagi2'><li>{l.data.catName}</li></Link>
                                             }
                                         </Fragment>
                                     ))
@@ -155,7 +156,7 @@ const Menu = ({ history }) => {
                                         <Fragment>
                                             {
                                                 l.data.catName === 'Rice' &&
-                                                <Link onClick={changeScreen} style={isActive(history, `/shop/${l.data.catName}`)} to={`/shop/${l.data.catName}`}><li>{l.data.catName}</li></Link>
+                                                <Link onClick={changeScreen} style={isActive(history, `/shop/${l.data.catName}`)} to={`/shop/${l.data.catName}`} className='bhaagi2'><li>{l.data.catName}</li></Link>
                                             }
                                         </Fragment>
                                     ))
@@ -171,7 +172,7 @@ const Menu = ({ history }) => {
                                         <Fragment>
                                             {
                                                 l.data.catName === 'Tiffin' &&
-                                                <Link onClick={changeScreen} style={isActive(history, `/shop/${l.data.catName}`)} to={`/shop/${l.data.catName}`}><li>{l.data.catName}</li></Link>
+                                                <Link onClick={changeScreen} style={isActive(history, `/shop/${l.data.catName}`)} to={`/shop/${l.data.catName}`} className='bhaagi2'><li>{l.data.catName}</li></Link>
                                             }
                                         </Fragment>
                                     ))
@@ -179,7 +180,7 @@ const Menu = ({ history }) => {
                             </Fragment>
                         }
 
-                        <Link onClick={changeScreen} style={isActive(history, '/cart')} to='/cart'><li><i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart</li></Link>
+                        <Link onClick={changeScreen} style={isActive(history, '/cart')} to='/cart' className='bhaagi2'><li><i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart<sup>{itemTotal()}</sup></li></Link>
                     </ul>
                 </div>
             </div>
