0a84a087b88847cc4de0ded270ba29990873b0fa
akashVerma <akashv000@gmail.com>
2020-12-30 21:00:20 +0530

    changes !!!

--- src/AdminPannel/Editorder.js
+++ src/AdminPannel/Editorder.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom'
 import { isAuth } from '../helpers/auth'
 import { toast, ToastContainer } from 'react-toastify'
 import '../Styles/adminPanel.css';
-import {Form} from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
+import { jsPDF } from 'jspdf'
 
 const Editorder = ({ match }) => {
     const [values, setValues] = useState({})
@@ -50,14 +51,31 @@ const Editorder = ({ match }) => {
         })
     }
 
+    const generatenow = () => {
+        if (values) {
+            const doc = new jsPDF()
+            doc.text(`${_id}`, 10, 10)
+            doc.text(`DeliveryTime - ${values.DeliveryTime}`, 10, 20)
+            doc.text(`status - ${values.Status}`, 10, 30)
+            doc.text(`UserId - ${values.UserID}`, 10, 40)
+            doc.text(`Grand Total - ${values.GrandTotal}`, 10, 50)
+            doc.text(`Quantity - ${values.Qty}`, 10, 60)
+            doc.text(`Order Type - ${values.Type}`, 10, 70)
+            doc.text(`Prices - ${values.Price}`, 10, 80)
+            doc.text(`Items - ${values.Items}`, 10, 90)
+            doc.text(`Address - ${values.Address}`, 10, 100)
+            doc.save('a40.pdf')
+        }
+    }
+
     return (
         <div>
             <ToastContainer />
 
             <div className="admin-panel-header">
-              <h5>Angadi.ae</h5>
-              <h2>Admin Panel</h2>
-              <button><i class="fa fa-power-off"/>  Logout</button>
+                <h5>Angadi.ae</h5>
+                <h2>Admin Panel</h2>
+                <button><i class="fa fa-power-off" />  Logout</button>
             </div>
 
             <div class="mobile_nav">
@@ -66,7 +84,7 @@ const Editorder = ({ match }) => {
                     <i class="fa fa-bars nav_btn"></i>
                 </div>
                 <div class="mobile_nav_items">
-                <Link className="admin1" to='/admin/dashboard'><i class="fa fa-desktop"></i>Dashboard</Link>
+                    <Link className="admin1" to='/admin/dashboard'><i class="fa fa-desktop"></i>Dashboard</Link>
                     <Link className="admin1" to='/get/orders'><i class="fa fa-cutlery"></i>orders</Link>
                     <Link className="admin1" to='/get/dishes'><i class="fa fa-glass"></i>dishes</Link>
                     <Link className="admin1" to='/get/category'><i class="fa fa-coffee"></i>category</Link>
@@ -84,72 +102,74 @@ const Editorder = ({ match }) => {
                     <h4>{isAuth().Name}</h4>
                 </div>
                 <Link className="admin1" to='/admin/dashboard'><i class="fa fa-desktop"></i>Dashboard</Link>
-                    <Link className="admin1" to='/get/orders'><i class="fa fa-cutlery"></i>orders</Link>
-                    <Link className="admin1" to='/get/dishes'><i class="fa fa-glass"></i>dishes</Link>
-                    <Link className="admin1" to='/get/category'><i class="fa fa-coffee"></i>category</Link>
-                    <Link className="admin1" to='/get/offers'><i class="fa fa-tag"></i>Offers</Link>
-                    <Link className="admin1" to='/get/users'><i class="fa fa-user"></i>Users</Link>
-                    <Link className="admin1" to='/create/category'><i class="fa fa-plus-square"></i>add category</Link>
-                    <Link className="admin1" to='/add/dish'><i class="fa fa-plus-square"></i>add dish</Link>
-                    <Link className="admin1" to='/add/offer'><i class="fa fa-plus-square"></i>add offer</Link>
+                <Link className="admin1" to='/get/orders'><i class="fa fa-cutlery"></i>orders</Link>
+                <Link className="admin1" to='/get/dishes'><i class="fa fa-glass"></i>dishes</Link>
+                <Link className="admin1" to='/get/category'><i class="fa fa-coffee"></i>category</Link>
+                <Link className="admin1" to='/get/offers'><i class="fa fa-tag"></i>Offers</Link>
+                <Link className="admin1" to='/get/users'><i class="fa fa-user"></i>Users</Link>
+                <Link className="admin1" to='/create/category'><i class="fa fa-plus-square"></i>add category</Link>
+                <Link className="admin1" to='/add/dish'><i class="fa fa-plus-square"></i>add dish</Link>
+                <Link className="admin1" to='/add/offer'><i class="fa fa-plus-square"></i>add offer</Link>
             </div>
 
             <div className='content1'>
                 {
                     values &&
                     <div>
-                      <div style={{display: "flex", justifyContent: "center", margin: "1em 0"}}>
-                        <Form.Control type="text" value={values.Status} style={{width: "400px"}}/>
-                        <select className="admin-order-utility-button" onChange={(e) => { setStatus(e.target.value) }}>
-                            <option>Please Select</option>
-                            <option value={'Awaiting Confirmation'}>Processing</option>
-                            <option value={'In Route'}>On The Way</option>
-                            <option value={'Order Delivered'}>Order Delivered</option>
-                            <option value={'Cancelled'}>Cancelled</option>
-                        </select>
-                        <button className="admin-order-utility-button" onClick={handleSubmit}>Change Status</button>
-                      </div>
-                        <table style={{width: "100%"}} className="edit-order-table">
-                          <tr>
-                            <th><i class="fa fa-tag" /> &nbsp; Order Id</th>
-                            <th><i class="fa fa-spinner" /> &nbsp; Status</th>
-                            <th><i class="fa fa-clock-o" /> &nbsp; Delivery Time</th>
-                          </tr>
-                          <tr>
-                            <td>{_id}</td>
-                            <td>{values.Status}</td>
-                            <td>{values.DeliveryTime}</td>
-                          </tr>
-                          <tr>
-                            <th><i class="fa fa-map-marker" /> &nbsp; Deliver to</th>
-                            <th><i class="fa fa-taxi" /> &nbsp; Type</th>
-                            <th><i class="fa fa-file-o" /> &nbsp; Grand Total</th>
-                          </tr>
-                          <tr>
-                            <td>{values.Address}</td>
-                            <td>{values.Type}</td>
-                            <td>{values.GrandTotal}</td>
-                          </tr>
+                        <div style={{ display: "flex", justifyContent: "center", margin: "1em 0" }}>
+                            <Form.Control type="text" value={values.Status} style={{ width: "400px" }} />
+                            <select className="admin-order-utility-button" onChange={(e) => { setStatus(e.target.value) }}>
+                                <option>Please Select</option>
+                                <option value={'Awaiting Confirmation'}>Processing</option>
+                                <option value={'In Route'}>On The Way</option>
+                                <option value={'Order Delivered'}>Order Delivered</option>
+                                <option value={'Cancelled'}>Cancelled</option>
+                            </select>
+                            <button className="admin-order-utility-button" onClick={handleSubmit}>Change Status</button>
+                        </div>
+
+                        <button onClick={generatenow}>Generate Pdf</button>
+                        <table style={{ width: "100%" }} className="edit-order-table">
+                            <tr>
+                                <th><i class="fa fa-tag" /> &nbsp; Order Id</th>
+                                <th><i class="fa fa-spinner" /> &nbsp; Status</th>
+                                <th><i class="fa fa-clock-o" /> &nbsp; Delivery Time</th>
+                            </tr>
+                            <tr>
+                                <td>{_id}</td>
+                                <td>{values.Status}</td>
+                                <td>{values.DeliveryTime}</td>
+                            </tr>
+                            <tr>
+                                <th><i class="fa fa-map-marker" /> &nbsp; Deliver to</th>
+                                <th><i class="fa fa-taxi" /> &nbsp; Type</th>
+                                <th><i class="fa fa-file-o" /> &nbsp; Grand Total</th>
+                            </tr>
+                            <tr>
+                                <td>{values.Address}</td>
+                                <td>{values.Type}</td>
+                                <td>{values.GrandTotal}</td>
+                            </tr>
                         </table>
-                        <div style={{fontWeight: "bolder", margin: "1em 0"}}>UserID -{values.UserID}</div>
+                        <div style={{ fontWeight: "bolder", margin: "1em 0" }}>UserID -{values.UserID}</div>
                         <div className="edit-order-data">
                             <div><h6>Dishes</h6> {values.Items && values.Items.map((d) => (
                                 <p>{d}</p>
                             ))}
-                            <p>Tax</p>
+                                <p>Tax</p>
                             </div>
                             <div><h6>Quantity</h6> {values.Qty && values.Qty.map((d) => (
                                 <p>{d}</p>
                             ))}
-                            <p>@ 5%</p>
+                                <p>@ 5%</p>
                             </div>
                             <div><h6>Price</h6> {values.Price && values.Price.map((d) => (
                                 <p>{d}</p>
                             ))}
-                            <p>{(values.GrandTotal * 0.05).toFixed(2)}</p>
-                            </div> 
+                                <p>{(values.GrandTotal * 0.05).toFixed(2)}</p>
+                            </div>
                         </div>
-                        
+
                     </div>
                 }
             </div>
